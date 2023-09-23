7b74bc6676c155fe8d70a80008c7dff5945bd851
Jonathan <90427766+JonathanAsf@users.noreply.github.com>
2023-09-23 09:27:33 -0300

    Update Conta.java - Saque
    Adição do método Saque para a conta
    O método está completamente funcional e exibe mensagens de êxito e falha na opreração.

--- Conta.java
+++ Conta.java
@@ -9,5 +9,17 @@ public class Conta {
 		void deposito(double valor) {
 			this.saldo += valor;
 		}
-	}
-
+		
+		public boolean saque(double valor) {
+			if(this.saldo >= valor) {
+				this.saldo -=  valor;
+				System.out.println("Saque efetuado");
+				System.out.println("Saldo atual da conta:" + this.saldo);
+				return true;
+			}else{
+				System.out.println("Saldo insuficiente para saldo");
+				return false;
+			}
+			
+		}
+}
