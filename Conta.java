7dffd88c2de118429714f55e99144344381218f2
Jonathan <90427766+JonathanAsf@users.noreply.github.com>
2023-09-24 11:16:29 -0300

    Update Conta.java
    Adição de getters e setters para os atributos agencia,saldo e numero.

--- Conta.java
+++ Conta.java
@@ -1,16 +1,16 @@
-package bytebank;
+package bytebank_composto;
 
 public class Conta {
-		double saldo;
-		int agencia;
-		int numero;
-		String titular;
-
-	//Método de depósito
+		private double saldo;
+		private int agencia;
+		private int numero;
+		public Cliente titular;
+		
+		//Metodo de depósito
 		void deposito(double valor) {
 			this.saldo += valor;
 		}
-	//Método de Saque
+		//Metodo de saque
 		public boolean saque(double valor) {
 			if(this.saldo >= valor) {
 				this.saldo -=  valor;
@@ -21,8 +21,9 @@ public class Conta {
 				System.out.println("Saldo insuficiente para saldo");
 				return false;
 			}
+			
 		}
-	//Método de Transferência
+		//Metodo de transferencia
 		public boolean transfere(double valor, Conta destino) {
 			if(saldo>=valor) {
 				this.saldo -= valor;
@@ -34,4 +35,26 @@ public class Conta {
 				return false;
 			}
 		}
+		//Metodo exibir saldo
+		public double getSaldo() {
+			return this.saldo;
+		}
+		
+		//Metodos agencia
+		public void setAgencia(int agencia) {
+			this.agencia = agencia;
+		}
+		public int getAgencia() {
+			return this.agencia;
+		}
+		//Metodo de set numero da conta
+		public void setNumero(int numero) {
+			this.numero = numero;
+		}
+		
+		public int getNumero() {
+			return this.numero;
+		}
+		
 }
+
